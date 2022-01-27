d7c4afab81bd81bbcaefc26b6715274134429683
Petr Fiedler <fiedlpe5@fit.cvut.cz>
2022-01-27 20:10:33 +0100

    routing: Create simple navbar for mobile view.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
 import { Home, Druhy, Poddruhy, Error } from "./pages";
 function App() {
     return (
         <BrowserRouter>
+            <Navbar></Navbar>
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/druhy" element={<Druhy />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FiMenu } from "react-icons/fi";
+
+const Navbar = () => {
+    const showSidebar = () => {
+        console.log("sidebar open");
+    };
+
+    return (
+        <Wrapper>
+            <h1>
+                <Link to="/">Mraky</Link>
+            </h1>
+            <button onClick={showSidebar}>
+                <FiMenu />
+            </button>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1rem;
+    border-bottom: 1px solid black;
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    button {
+        border: none;
+        background-color: transparent;
+        font-size: 2rem;
+        padding: 1rem 0;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+    }
+`;
+
+export default Navbar;
