0fb479358a07bb76648f6c7a964c49b56ca69ca0
Petr Fiedler <fiedlpe5@fit.cvut.cz>
2022-01-28 20:39:43 +0100

    errorpage: Add errorpage.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,55 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FiAlertTriangle, FiChevronLeft } from "react-icons/fi";
 
 const Error = () => {
-    return <main>Error</main>;
+    return (
+        <Wrapper>
+            <FiAlertTriangle className="triangle" />
+            <h1>Stránka nenalezena.</h1>
+            <Link to="/">
+                <FiChevronLeft />
+                Zpět na hlavní stránku
+            </Link>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: calc(calc(100vh - 10vw) - 25rem);
+
+    h1 {
+        font-size: 3rem;
+    }
+
+    .triangle {
+        color: var(--clr2);
+        font-size: 5rem;
+    }
+
+    a {
+        color: var(--clr2);
+        padding: 1rem;
+        font-size: 2rem;
+        border: 3px solid var(--clr2);
+        text-decoration: none;
+        border-radius: 0.5rem;
+        display: flex;
+        align-items: center;
+        svg {
+            margin-right: 1rem;
+        }
+
+        :hover {
+            background-color: var(--clr2);
+            color: var(--clr1);
+        }
+    }
+`;
 
 export default Error;
