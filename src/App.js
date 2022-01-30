8875563823f3666904fe2e9832728c575363c4fc
Petr Fiedler <fiedlpe5@fit.cvut.cz>
2022-01-30 16:40:14 +0100

    routing: Make page scroll to top on router change.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 import { Home, Druhy, Poddruhy, Error } from "./pages";
+import ScrollToTop from "./utils/ScrollToTop";
 function App() {
     return (
         <BrowserRouter>
             <Sidebar />
             <Navbar />
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/druhy" element={<Druhy />} />
-                <Route exact path="/poddruhy" element={<Poddruhy />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
+            <ScrollToTop>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/druhy" element={<Druhy />} />
+                    <Route exact path="/poddruhy" element={<Poddruhy />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </ScrollToTop>
             <Footer />
         </BrowserRouter>
     );
