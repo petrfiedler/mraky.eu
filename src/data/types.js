22ae6ba2a7e01fa24bec8e7a6ec9745478e53486
Petr Fiedler <petr1fiedler@gmail.com>
2022-02-14 12:27:29 +0100

    content: Add some cloud type articles.

--- src/data/types.js
+++ src/data/types.js
@@ -47,6 +47,30 @@ const types = [
             "Tenké, menší nebo větší skupiny nebo vrstvy bílých oblaků bez vlastního stínu, složené z velmi malých oblačných částí v podobě zrnek nebo vlnek apod.",
         level: "high",
         img: "https://whatsthiscloud.com/wp-content/uploads/2018/03/cirrocumulus-floccus-1040x585.jpg",
+        article: [
+            {
+                type: "text",
+                text: "Cirrocumulus, česky řasokupa, je oblak vysokého patra. Je mezinárodně definován jako tenké, menší nebo větší skupiny nebo vrstvy bílých oblaků bez vlastního stínu, složené 16 z velmi malých oblačných částí v podobě zrnek nebo vlnek apod. Díky nízkým teplotám ve své výšce se skládá skoro pouze z ledových krystalků, případně z kapiček přechlazené vody. Cirrocumulus označujeme zkratkou Cc. Jedná se o jeden z nejméně četných druhů oblaků.",
+            },
+            {
+                type: "image",
+                src: "https://whatsthiscloud.com/wp-content/uploads/2019/03/cirrocumulus-undulatus.jpg",
+                alt: "Cirrocumulus stratiformis undulatus",
+            },
+            {
+                type: "text",
+                text: "Na rozdíl od cirru a cirrostratu má cirrocumulus kupovitý charakter. Tím je poměrně dobře rozpoznatelný od ostatních druhů oblaků ve vysokém patře. Jeho vzhled často připomíná zrnka rýže. Může však mít i podobu vlnek připomínající rybí kostru (undulatus). Na rozdíl od cirru je v tomto případě ucelenější. Na cirrocumulu nemůžeme pozorovat halové jevy, na rozdíl od ostatních oblaků vysokého patra. Můžeme na něm však vzácně spatřit jiné optické jevy, jako je například irizace nebo koróna.",
+            },
+            {
+                type: "image",
+                src: "https://whatsthiscloud.com/wp-content/uploads/2018/03/cirrocumulus-stratiformis-1040x585.jpg",
+                alt: "Cirrocumulus stratiformis",
+            },
+            {
+                type: "text",
+                text: "Cirrocumulus může vzniknout konvekcí způsobenou vlivem orografie, tedy vertikálními pohyby vlhkého vzduchu podle tvaru zemského povrchu. Tento oblak označujeme cirrocumulem pouze tehdy, vytvořil-li se z cirrostratu, nebo cirru, nebo je-li s některým z nich viditelně spojený. Za cirrocumulus nepovažujeme oblak s neúplně vyvinutými prvky struktury.",
+            },
+        ],
     },
     {
         id: "cirrostratus",
@@ -55,6 +79,30 @@ const types = [
             "Průsvitný bělavý oblačný závoj vláknitého nebo hladkého vzhledu, který úplně, nebo částečně zakrývá oblohu a dává vznik halovým jevům.",
         level: "high",
         img: "https://whatsthiscloud.com/wp-content/uploads/2018/03/cirrostratus-fibratus-undulatus-halo-1040x585.jpg",
+        article: [
+            {
+                type: "text",
+                text: "Cirrostratus, v češtině řasová sloha, je oblak vysokého patra. Označujeme ho zkratkou Cs. Je charakterizován Světovou meteorologickou organizací jako průsvitný bělavý oblačný závoj vláknitého nebo hladkého vzhledu, který úplně, nebo částečně zakrývá oblohu a dává vznik halovým jevům. Stejně jako ostatní oblaka vysokého patra, se cirrostratus skládá převážně z ledových krystalků.",
+            },
+            {
+                type: "image",
+                src: "https://whatsthiscloud.com/wp-content/uploads/2019/03/cirrostratus-nebulosus.jpg",
+                alt: "Cirrostratus nebulosus",
+            },
+            {
+                type: "text",
+                text: "Cirrostratus je výrazně průsvitný, což je zapříčiněno malou velikostí krystalků, ze kterých se skládá, a také tloušťkou samotného oblaku. Díky tomu je Slunce přes cirrostratus takřka vždy viditelné. Občas se vytvoří krystalky natolik velké, že dosahují konečné rychlosti, což může vytvořit vláknitou strukturu cirrostratu (fibratus). Cirrostratus může být i tak tenký, že je až nemožné ho okem spatřit. V takovém případě mohou k indikaci pomoci halové jevy, ke kterým nám cirrostratus dává ze všech druhů oblaků nejlepší podmínky.",
+            },
+            {
+                type: "image",
+                src: "https://whatsthiscloud.com/wp-content/uploads/2018/03/cirrostratus-fibratus-halo-1040x585.jpg",
+                alt: "Cirrostratus fibratus s 22° halem",
+            },
+            {
+                type: "text",
+                text: "Cirrostratus se může vytvořit díky pomalu stoupajícímu větru. Často se vytváří na čele frontálních systémů. Stejně jako všechna oblaka vysokého patra se může také vyvinout z kondenzačních čar letadel (homomutatus). Cirrostratus se může vytvořit splynutím prvků cirru nebo cirrocumulu. Z cirrocumulu se také může vytvořit díky odpadávání ledových krystalků. Může se vytvořit ztenčením altostratu nebo rozšířením horní části kovadliny cumulonimbu.",
+            },
+        ],
     },
     {
         id: "altocumulus",
@@ -63,6 +111,7 @@ const types = [
             "Menší nebo větší skupiny nebo vrstvy oblaků barvy bílé či šedé, popř. bílé a šedé, které mají vlastní stíny.",
         level: "middle",
         img: "https://whatsthiscloud.com/wp-content/uploads/2018/03/altocumulus-stratiformis-undulatus-1040x585.jpg",
+        article: [],
     },
     {
         id: "altostratus",
@@ -71,6 +120,7 @@ const types = [
             "Šedavá a modravá oblačná plocha nebo vrstva s vláknitou nebo žebrovitou strukturou nebo i bez patrné struktury, která pokrývá úplně nebo částečně oblohu.",
         level: "middle",
         img: "https://whatsthiscloud.com/wp-content/uploads/2018/03/altostratus-translucidus-1-1040x585.jpg",
+        article: [],
     },
     {
         id: "nimbostratus",
@@ -79,6 +129,7 @@ const types = [
             "Šedá, často tmavá oblačná vrstva nebo plocha, která má vlivem vypadávání více méně trvalých dešťových nebo sněhových srážek matný rozplývavý vzhled.",
         level: "middle",
         img: "https://whatsthiscloud.com/wp-content/uploads/2019/03/nimbostratus-praecipitatio.jpg",
+        article: [],
     },
     {
         id: "stratocumulus",
@@ -87,6 +138,7 @@ const types = [
             "Bílé nebo šedobílé vrsvy nebo skupiny oblaků, které mají téměř vždy tmavá místa.",
         level: "low",
         img: "https://whatsthiscloud.com/wp-content/uploads/2019/03/stratocumulus-sunset.jpg",
+        article: [],
     },
     {
         id: "stratus",
@@ -95,6 +147,7 @@ const types = [
             "Šedá oblačná vrstva s dosti jednotvárnou základnou, z níž může vypadávat mrholení, popř. ledové jehličky nebo sněhová zrna.",
         level: "low",
         img: "https://whatsthiscloud.com/wp-content/uploads/2019/03/stratus-nebulosus1.jpg",
+        article: [],
     },
     {
         id: "cumulus",
@@ -103,6 +156,7 @@ const types = [
             "Osamocený oblak, obvykle hustý a s ostře ohraničenými obrysy, vyvíjející se směrem vzhůru ve tvaru kup, kupolí nebo věží.",
         level: "low",
         img: "https://whatsthiscloud.com/wp-content/uploads/2018/03/cumulus-2-1040x585.jpg",
+        article: [],
     },
     {
         id: "cumulonimbus",
@@ -111,6 +165,7 @@ const types = [
             "Mohutný a hustý oblak velkého vertikálního rozsahu v podobě hor nebo obrovských věží.",
         level: "low",
         img: "https://whatsthiscloud.com/wp-content/uploads/2018/03/cumulonimbus-capillatus-incus-1040x585.jpg",
+        article: [],
     },
 ];
 
